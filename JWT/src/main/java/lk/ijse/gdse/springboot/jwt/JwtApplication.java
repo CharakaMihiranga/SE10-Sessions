7cdf2167fb8614abe018b248a2f50d873209b92d
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-08-23 22:07:03 +0530

    JWT project init

---
+++ JWT/src/main/java/lk/ijse/gdse/springboot/jwt/JwtApplication.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.springboot.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApplication.class, args);
+    }
+
+}
